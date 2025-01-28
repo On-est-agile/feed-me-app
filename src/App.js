b2e0b2ab9342d009665437e8ac122849ce08fb05
Enimoop <marine.forcioli@laposte.net>
2025-01-28 23:04:54 +0100

    Added connection to broker in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Home from "./pages/Home";
 import DeviceInfo from "./pages/DeviceInfo";
+import { connectMqtt, subscribeToTopic } from ".//services/MqttHandler.js";
 
 function App() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    // 1. Connexion au broker MQTT
+    connectMqtt("wss://test.mosquitto.org:8081");
+
+    // 2. S'abonner au topic Gamelle1/remplissage
+    subscribeToTopic("Gamelle1/remplissage", (receivedMessage) => {
+      console.log("Message reçu : ", receivedMessage);
+      setMessage(receivedMessage); 
+    });   
+  }, []);
+
   return (
     <Router>
       <Routes>
