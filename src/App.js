0a3dd83449a1f3a1301da8f7a9db43b36e0d9e4b
Enimoop <marine.forcioli@laposte.net>
2025-01-29 13:32:41 +0100

    suscribe to topic to receive device status

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Home from "./pages/Home";
 import DeviceInfo from "./pages/DeviceInfo";
-import { connectMqtt, subscribeToTopic } from ".//services/MqttHandler.js";
+import { connectMqtt, publishMessage, subscribeToTopic } from ".//services/MqttHandler.js";
 
 function App() {
   const [message, setMessage] = useState("");
+  const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
   useEffect(() => {
     // 1. Connexion au broker MQTT
     connectMqtt("wss://test.mosquitto.org:8081");
 
     // 2. S'abonner au topic Gamelle1/remplissage
-    subscribeToTopic("Gamelle1/remplissage", (receivedMessage) => {
+    subscribeToTopic(`feedme/${CLIENT_SECRET}/statuses/balance_bottom`, (receivedMessage) => {
       console.log("Message reçu : ", receivedMessage);
       setMessage(receivedMessage); 
     });   
@@ -22,8 +23,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/device-info" element={<DeviceInfo />} />
+        <Route path="/home" element={<Home message={message} />} />
+        <Route path="/device-info/:id" element={<DeviceInfo message={message} />} />
       </Routes>
     </Router>
   );
