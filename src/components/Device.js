d86c0af77ad9cf1517d401efb6f33afec1968381
Enimoop <marine.forcioli@laposte.net>
2025-01-30 13:22:01 +0100

    Moved Device style to index.css

--- src/components/Device.js
+++ src/components/Device.js
@@ -26,55 +26,14 @@ function Device({ device, message }) {
   };
 
   return (
-    <div style={styles.deviceContainer} onClick={handleClick}>
-      <h2 style={styles.title}>{deviceName}</h2>
-      <p style={styles.percentage}>Remplissage : {amountValue}</p>
-      <p style={deviceUid ? styles.deviceUid : styles.unpairedDevice}>
+    <div className="device-container" onClick={handleClick}>
+      <h2 className="device-title">{deviceName}</h2>
+      <p className="device-percentage">Remplissage : {amountValue}</p>
+      <p className={deviceUid ? "device-uid" : "device-unpaired"}>
         {deviceUid ? "✔️ Appairé" : "❌ Device non appairé"}
       </p>
     </div>
   );
 }
-
-const styles = {
-  deviceContainer: {
-    width: "150px",
-    height: "150px",
-    border: "2px solid #ADD8E6",
-    borderRadius: "10px",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f9f9f9",
-    boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
-    cursor: "pointer",
-    padding: "10px",
-    textAlign: "center",
-  },
-  title: {
-    fontSize: "1.2rem",
-    fontWeight: "bold",
-    margin: "0",
-    color: "#333",
-    textTransform: "capitalize", // 🔹 Première lettre en majuscule
-  },
-  percentage: {
-    fontSize: "0.9rem",
-    color: "#666",
-    marginTop: "10px",
-  },
-  deviceUid: {
-    fontSize: "0.9rem",
-    color: "green", // ✅ Couleur verte si appairé
-    marginTop: "5px",
-  },
-  unpairedDevice: {
-    fontSize: "0.9rem",
-    color: "red", // ❌ Couleur rouge si non appairé
-    fontWeight: "bold",
-    marginTop: "5px",
-  },
-};
 
 export default Device;
